ecd3d7d22bb9dfb954ad89e6972d437d1b66d428
wagner <wagnersanches5@hotmail.com>
2026-01-07 21:19:29 -0300

    add .gitignore

--- main.go
+++ main.go
@@ -4,11 +4,10 @@ import (
 	"fmt"
 )
 
+const MY_PI = 3
+
 func main() {
 	s := "gopher"
 	fmt.Printf("Hello and welcome, %s!\n", s)
 
-	for i := 1; i <= 5; i++ {
-		fmt.Println("i =", 100/i)
-	}
 }
